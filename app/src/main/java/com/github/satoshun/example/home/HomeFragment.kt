4bf9f24b9e2b5af852baef5311a4126673b82474
SatoShun <shun.sato1@gmail.com>
2019-11-09 04:18:07 +0000

    basic

--- app/src/main/java/com/github/satoshun/example/home/HomeFragment.kt
+++ app/src/main/java/com/github/satoshun/example/home/HomeFragment.kt
@@ -24,7 +24,7 @@ class HomeFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    binding.submit.setOnClickListener {
+    binding.basic.setOnClickListener {
       findNavController().navigate(R.id.nav_basic_dialog)
     }
 
