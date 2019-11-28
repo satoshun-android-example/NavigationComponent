346d2aa5acb95343e5739cb228ca013f62dd6adc
SatoShun <shun.sato1@gmail.com>
2019-11-28 03:05:28 +0000

    toolbar handle

--- app/src/main/java/com/github/satoshun/example/AppActivity.kt
+++ app/src/main/java/com/github/satoshun/example/AppActivity.kt
@@ -2,6 +2,8 @@ package com.github.satoshun.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.github.satoshun.example.databinding.AppActBinding
 
 class AppActivity : AppCompatActivity() {
@@ -11,5 +13,22 @@ class AppActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = AppActBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    setSupportActionBar(binding.toolbar)
+//    binding.toolbar.setupWithNavController(findNavController(R.id.nav_host_fragment))
+//    lifecycleScope.launch {
+//      delay(3000)
+//    }
+
+    val navController = findNavController(R.id.nav_host_fragment)
+    setupActionBarWithNavController(navController)
+    navController.addOnDestinationChangedListener { controller, destination, arguments ->
+      if (destination.id == R.id.nav_home) {
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_close_black_24dp)
+      } else {
+        supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+      }
+    }
   }
 }
