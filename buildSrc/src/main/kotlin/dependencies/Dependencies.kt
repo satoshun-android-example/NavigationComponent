ccddaeda8263e63df83b479c3e7a957726811e3b
SatoShun <shun.sato1@gmail.com>
2020-07-29 01:15:58 +0000

    update

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -4,10 +4,10 @@ const val COMPILE_SDK = 29
 const val MIN_SDK = 21
 const val TARGET_SDK = 29
 
-private const val VKOTLIN = "1.3.61"
-private const val VNAVIGATION = "2.2.0-rc02"
+private const val VKOTLIN = "1.3.71"
+private const val VNAVIGATION = "2.2.0"
 
-const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.6.0-beta04"
+const val ANDROID_PLUGIN = "com.android.tools.build:gradle:4.0.1"
 const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$VKOTLIN"
 const val SAFEARGS_PLUGIN =
   "androidx.navigation:navigation-safe-args-gradle-plugin:$VNAVIGATION"
@@ -29,11 +29,7 @@ const val CARDVIEW = "androidx.cardview:cardview:1.0.0"
 
 const val FLEXBOX = "com.google.android:flexbox:1.1.0"
 
-const val CONTENTPAGER = "androidx.contentpager:contentpager:1.0.0-rc01"
-const val WEBKIT = "androidx.webkit:webkit:1.0.0"
-const val MEDIA2 = "androidx.media2:media2:1.0.0-alpha04"
-
-const val MATERIAL = "com.google.android.material:material:1.1.0-beta02"
+const val MATERIAL = "com.google.android.material:material:1.1.0"
 const val PAGING = "androidx.paging:paging-runtime:2.1.0"
 const val BROWSER = "androidx.browser:browser:1.0.0"
 
@@ -41,35 +37,27 @@ const val NAVIGATION = "androidx.navigation:navigation-common-ktx:$VNAVIGATION"
 const val NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:$VNAVIGATION"
 const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:$VNAVIGATION"
 
-private const val VROOM = "2.1.0-alpha04"
+private const val VROOM = "2.1.0"
 const val ROOM_COMMON = "androidx.room:room-common:$VROOM"
 const val ROOM_RUNTIME = "androidx.room:room-runtime:$VROOM"
 const val ROOM_COMPILER = "androidx.room:room-compiler:$VROOM"
 
-const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0-alpha02"
-const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata-ktx:2.2.0-alpha02"
-const val LIFECYCLE_COMPILER = "androidx.lifecycle:lifecycle-compiler:2.0.0"
-const val LIFECYCLE_EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:2.2.0-alpha02"
-const val LIFECYCLE_RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:2.2.0-alpha02"
-
-const val BIOMETRIC = "androidx.biometric:biometric:1.0.0-alpha03"
+const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0"
+const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata-ktx:2.2.0"
+const val LIFECYCLE_COMPILER = "androidx.lifecycle:lifecycle-compiler:2.2.0"
+const val LIFECYCLE_EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:2.2.0"
+const val LIFECYCLE_RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:2.2.0"
 
 const val EMOJI = "androidx.emoji:emoji:1.0.0"
 const val EMOJI_COMPAT = "androidx.emoji:emoji-appcompat:1.0.0"
 const val EMOJI_BUNDLED = "androidx.emoji:emoji-bundled:1.0.0"
 
-const val SAVEDSTATE = "androidx.savedstate:savedstate:1.0.0-alpha02"
-const val SAVEDSTATE_VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-savedstate:1.0.0-alpha01"
+const val SAVEDSTATE = "androidx.savedstate:savedstate:1.0.0"
+const val SAVEDSTATE_VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-savedstate:1.0.0"
 
-const val VIEWPAGER2 = "androidx.viewpager2:viewpager2:1.0.0-alpha01"
+const val VIEWPAGER2 = "androidx.viewpager2:viewpager2:1.0.0"
 
 const val RECOMMENDATION = "androidx.recommendation:recommendation:1.0.0"
-
-const val PREFERENCE = "androidx.preference:preference-ktx:1.1.0-alpha03"
-
-const val MULTIDEX = "androidx.multidex:multidex:2.0.0"
-
-const val RXWEBVIEW = "com.github.satoshun.RxWebView:rxwebview-kotlin:2.3.0"
 
 const val RXJAVA = "io.reactivex.rxjava2:rxjava:2.2.0"
 const val RXANDROID = "io.reactivex.rxjava2:rxandroid:2.1.0"
@@ -77,7 +65,7 @@ const val RXANDROID = "io.reactivex.rxjava2:rxandroid:2.1.0"
 const val GROUPIE = "com.xwray:groupie:2.3.0"
 const val GROUPIE_DATABINDING = "com.xwray:groupie-databinding:2.3.0"
 
-const val WORK_MANAGER = "androidx.work:work-runtime-ktx:2.0.0-rc01"
+const val WORK_MANAGER = "androidx.work:work-runtime-ktx:2.0.0"
 
 const val ANDROID_ANNOTATION = "androidx.annotation:annotation:1.0.0"
 
